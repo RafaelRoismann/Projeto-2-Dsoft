084d633ab13655ebee89dbe3838ef6b49bca17a7
GustavoTavaresBraga <gustavobraga1706@gmail.com>
2023-04-26 17:48:57 -0300

    Preenche frota

--- Projeto_2.py
+++ Projeto_2.py
@@ -1,15 +1,27 @@
 # EP2
 
 # Define posições
-def define_posicoes(linha,coluna,orientacao,tamanho):
+def define_posicoes(linha, coluna, orientacao, tamanho):
     lista_posicoes = []
 
     for i in range(tamanho): 
         if orientacao == "vertical":
-            lista_posicoes.append([linha,coluna])
+            lista_posicoes.append([linha, coluna])
             linha += 1 
             
         if orientacao == "horizontal":
-            lista_posicoes.append([linha,coluna])
+            lista_posicoes.append([linha, coluna])
             coluna += 1 
+
     return lista_posicoes
+
+# Preenche frota
+def preenche_frota(frota, navio, linha, coluna, orientacao, tamanho):
+    novo_navio = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    if navio not in frota:
+        frota[navio] = []
+
+    frota[navio].append(novo_navio)
+
+    return frota
